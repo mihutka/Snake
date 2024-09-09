7c6482a1dec4f927464382c2b9a9c9041ab27053
mihail <opilane@HP-CZC2349HT9>
2024-09-09 14:52:38 +0300

    Add project files.

---
+++ mainClass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    internal class mainClass
+    {
+        static void Main(string[] args)
+        {
+            Point p1 = new Point(1,3, '*'); /*loob uue punkti p1*/
+            /*p1.x = 1;
+            p1.y = 3;
+            p1.sym = '*';*/
+            p1.Draw(); /*tõmbab punkti*/
+
+            Point p2 = new Point(4,5,'#');
+            /*p2.x = 4;                 /*muutujad*/
+           /* p2.y = 5;
+            p2.sym = '#';
+            p2.Draw();*/
+
+          
+
+
+            Console.ReadLine();
+        }
+        
+    }
+}
